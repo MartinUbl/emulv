fb7b5c9e604bcbd5bc90a6ce199f3f9f43110f53
jonasx <jonasx.dufek@gmail.com>
2023-03-05 23:25:12 +0100

    Basic libricsv setup. Can run fib.rv64.elf.

---
+++ emulator/cmake-build-debug/_deps/libriscv-subbuild/CMakeFiles/libriscv-populate.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libriscv-populate"
+  "CMakeFiles/libriscv-populate-complete"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-build"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-configure"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-download"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-install"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-mkdir"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-patch"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-test"
+  "libriscv-populate-prefix/src/libriscv-populate-stamp/libriscv-populate-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/libriscv-populate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ emulator/cmake-build-debug/_deps/libriscv-subbuild/CMakeFiles/libriscv-populate.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for libriscv-populate.
+# This may be replaced when dependencies are built.
