6c647b261780ae13f6fe133a85929490d10cf202
HynekM99 <hynekmoudry@gmail.com>
2023-05-01 22:49:43 +0200

    UART event now inherits from PeripheralDeviceEvent

--- src/utils/events/uart_event.cpp
+++ src/utils/events/uart_event.cpp
@@ -3,16 +3,3 @@
 //
 
 #include "uart_event.h"
-
-eventType uart_event::getType() {
-    return eventType::UART_event;
-}
-
-uint32_t uart_event::getData() {
-    return data_;
-}
-
-uart_event::uart_event(uint32_t data) {
-    data_ = data;
-}
-
--- src/utils/events/uart_event.h
+++ src/utils/events/uart_event.h
@@ -7,18 +7,29 @@
 
 #include <string>
 #include "AbstractEvent.h"
+#include "PeripheralDeviceEvent.h"
+#include "../../modules/uart.h"
 
 const std::string UART_event_description {"uart_message_received"};
 
-class uart_event : public AbstractEvent {
+class uart_event : public PeripheralDeviceEvent {
 private:
     uint32_t data_;
 public:
-    eventType getType() override;
-    ~uart_event() = default;
-    uint32_t getData();
+    uart_event(const modules::UART_Device &uart, uint32_t data) :
+        PeripheralDeviceEvent(uart),
+        data_(data)
+    {}
 
-    uart_event(uint32_t data);
+    ~uart_event() = default;
+
+    eventType getType() override {
+        return eventType::UART_event;
+    }
+
+    uint32_t getData() {
+        return data_;
+    }
 };
 
 
